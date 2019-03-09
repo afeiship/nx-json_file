53eb657aed346d68a393ac7e334b7cd3dc202bbc
afeiship <1290657123@qq.com>
2019-03-10 01:44:13 +0800

    fix: in module

--- lib/nx/json_file.rb
+++ lib/nx/json_file.rb
@@ -1,21 +1,23 @@
 require "nx"
 require "json"
 
-module JsonFile
-  def initialize(filname)
-    @filename = filname
-    @json = nil
-  end
+module Nx
+  class JsonFile
+    def initialize(filname)
+      @filename = filname
+      @json = nil
+    end
 
-  def load
-    @json = JSON.parse File.read(@filename)
-  end
+    def load
+      @json = JSON.parse File.read(@filename)
+    end
 
-  def update(path, value)
-    @json.set(path, value)
-  end
+    def update(path, value)
+      @json.set(path, value)
+    end
 
-  def save(target)
-    File.write(target, JSON.pretty_generate(@json))
+    def save(target)
+      File.write(target, JSON.pretty_generate(@json))
+    end
   end
 end
